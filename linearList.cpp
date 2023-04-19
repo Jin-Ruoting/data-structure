00a339e5160effa0e9c39e3d2556a1c82303cf74
Jin-Ruoting <ruotingjin@gmail.com>
2023-04-19 21:50:35 +0800

    add class DLinkList in linearList.cpp

--- linearList.cpp
+++ linearList.cpp
@@ -59,7 +59,7 @@ template <typename DataType>
 void SeqList<DataType>::PrintList()
 {
     for (int i = 0; i < length; i++)
-        cout << data[i] << "\t";
+        cout << data[i] << " ";
     cout << endl;
 }
 
@@ -391,16 +391,151 @@ LinkList<DataType>:: ~LinkList()
     }    
 }
 
-//双链表
+//双链表结点结构定义
 template <typename DataType>
-struct DulNode
+struct DNode
 {
-    void Insert(int i, DataType x);     //在双链表第 i 个位置插入值为 x 的结点
-    DataType Delete(int i);             //删除双链表中第 i 个节点
-    DataType data;                      //初始化数据域
-    DulNode<DataType> *prior, *next;    //初始化前驱和后继指针
+    DataType data;                  //初始化数据域
+    DNode<DataType> *prior, *next;  //初始化前驱指针和后继指针
 };
 
+template <typename DataType>
+class DLinkList
+{
+public:
+    DLinkList();
+    ~DLinkList();
+    int Length();
+    DataType Get(int i);
+    int Locate(DataType x);
+    void InsertNextDNode(DNode<DataType> *p, DNode<DataType> *s);
+    DataType DeleteDNode(DNode<DataType> *p);
+    int Empty();
+    void PrintDLinkList();
+private:
+    DNode<DataType> *first;                                         //双链表的头指针
+};
+
+//双链表无参构造函数
+template <typename DataType>
+DLinkList<DataType>::DLinkList()
+{
+    first = new Node<DataType>; //分配一个头结点
+    first->prior = nullptr;     //头结点的 prior 指针永远为空指针
+    first->next = nullptr;      //头结点后继结点暂时为空
+}
+
+//析构函数，循环释放各个结点
+template <typename DataType>
+DLinkList<DataType>::~DLinkList()
+{
+    while (first->next != nullptr)
+        DeleteDNode(first->next);   //删除头结点的后继结点
+    delete(first);                  //释放头结点存储空间
+    first = nullptr;                //头指针置空
+}
+
+//求双链表的长度并以 int 形式返回
+template <typename DataType>
+int DLinkList<DataType>::Length()
+{
+    DNode<DataType> *p = first->next;
+    int count = 0;
+    while (p != nullptr)                //遍历双链表
+    {
+        p = p->next;
+        count++;
+    }
+    return count;
+}
+
+//按位查找双链表中第 i 个元素的数据域的值（设头结点是第 0 位）
+template <typename DataType>
+DataType DLinkList<DataType>::Get(int i)
+{
+    DNode<DataType> *p = first->next;   //初始化工作指针 p 为头结点的后继
+    int n = 1;                          //初始化计数器从第一个结点开始计算
+    while (p != nullptr)
+    {
+        if (n == i)                     //当前计数器值与需要查找的元素的位数相同时即为查找成功
+            return p->data;             //返回查找到的结点的数据域
+        else
+        {
+            i++;
+            p = p->next;
+        }
+    }
+    throw "查找位置非法";
+}
+
+//按值查找双链表中第一个数据域与待查找值相同的元素的位数
+template <typename DataType>
+int DLinkList<DataType>::Locate(DataType x)
+{
+    DNode<DataType> *p = first->next;
+    int count = 1;
+    while (p != nullptr)
+    {
+        if (p->data == x)
+            return count;
+        else
+        {
+            count++;
+            p->next;
+        }
+    }
+    throw "没有找到与带查找值相同的结点数据域";
+}
+
+//在 p 结点之后插入 s 结点
+template <typename DataType>
+void DLinkList<DataType>::InsertNextDNode(DNode<DataType> *p, DNode<DataType> *s)
+{
+    if (p == nullptr || s == nullptr)
+        throw "非法参数";
+    s->next = p->next;
+    if (p->next != nullptr)
+        p->next->prior = s;
+    s->prior = p;
+    p->next = s;
+    cout << "成功在 " << p->data << " 之后插入 " << s->data << endl;
+}
+
+//删除指针 p 指向的结点，并返回节点中存储的数据
+template <typename DataType>
+DataType DLinkList<DataType>::DeleteDNode(DNode<DataType> *p)
+{
+    if (p == nullptr || p->prior == nullptr)    //当 p 为空或 p 为头结点时无法删除
+        throw "非法结点";
+    DataType data = p->data;                    //存储被删结点中的数据
+    (p->prior)->next = p->next;                 //将 p 的前驱结点的后继指针指向 p 的后继结点
+    if (p->next != nullptr)                     //当 p 为双链表中最后一位时 next 指向空指针，空指针不存在前驱结点
+        (p->next)->prior = p->prior;            //当 p 不为双链表中最后一位时 p 的后继结点的前驱指针指向 p 的前驱结点
+    delete(p);                                  //释放 p 的存储空间
+    return data;                                //返回被删除节点中存储的数据
+}
+
+//判断双链表是否为空，若为空则返回 1
+template <typename DataType>
+int DLinkList<DataType>::Empty()
+{
+    if (first->next = nullptr)
+        return 1;
+    return 0;
+}
+
+//遍历双链表，打印链表中数据域
+template <typename DataType>
+void DLinkList<DataType>::PrintDLinkList()
+{
+    DNode<DataType> *p = first->next;       //初始化工作指针 p
+    while (p != nullptr)
+    {
+        cout << p->data << " ";
+        p = p->next;                        //工作指针 p 后移
+    }
+    cout << endl;
+}
 
 int main()
 {
