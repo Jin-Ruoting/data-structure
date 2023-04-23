69696ca683393231004c46009d0eedf4e41edd15
Jin-Ruoting <ruotingjin@gmail.com>
2023-04-23 18:14:09 +0800

    Completed the basic part of the Linear List

--- 2. Linear List/linearList.cpp
+++ 2. Linear List/linearList.cpp
@@ -841,6 +841,110 @@ DataType f_2_3_10(Node<DataType> &A)
     return B;                               //返回序号为奇数的表 B，偶数表 A 为引用参数，可直接使用
 }
 
+//2.3.11 (medium) 设C={a1,b1,a2,b2,…，an,bn}为线性表，采用带头结点的单链表存放，设计一个就地算法，将其拆分为两个线性表，使得A={a1,a2,…,an}，B={bn,…,b2,b1}
+template <typename DataType>
+DataType f_2_3_11(Node<DataType> &A)
+{
+    Node<DataType> B = new Node<DataType>;  //初始化 B 链表
+    B->next = nullptr;                      //链表 B 为空表
+    Node<DataType> *p = A->next;            //工作指针
+    Node<DataType> *ra = A;                 //新链表 A 的尾指针
+    Node<DataType> *q;                      //用于保存指针 p 的后继结点
+    while (p != nullptr)                    //一次循环处理两个结点
+    {
+        ra = p;                             //1 号位不移入表 B，后移
+        p = p->next;                        //工作指针后移
+        q = p->next;                        //保存待移出结点的后续结点，防止断链
+        p->next = B->next;                  //头插法将操作结点插入链表 B
+        B->next = p;
+        ra->next = p->next;                 //链表 A 重新链接
+        p = q;                              //工作指针后移
+    }
+    ra->next = nullptr;                     //结束循环后将表 A 尾指针指向空指针
+    return B;                               //返回链表 B
+}
+
+//2.3.12 (medium) 在一个递增有序的线性表中，有数值相同的元素存在。若存储方式为单链表，设计算法去掉数值相同的元素，使表中不再有重复的元素，例如(7, 10, 10, 21, 30, 42, 42, 42, 51, 70)将变为(7, 10, 21, 30, 42, 51, 70)
+template <typename DataType>
+void f_2_3_12(Node<DataType> &L)
+{
+    Node<DataType> *p = L->next;    //工作指针
+    Node<DataType> *pre = L;        //和工作指针做对比的基准指针
+    while (p != nullptr)
+    {
+        if (p->data == pre->data)
+        {
+            pre->next = p->next;    //摘链
+            delete(p);
+            p = pre->next;          //后移工作指针，准备对比下一元素
+        }
+        else                        //若元素不相等
+        {
+            pre = p;                //后移工作指针和基准指针
+            p = p->next;
+        }
+    }    
+}
+
+//2.3.14 (medium) 设A和B是两个单链表（带头结点），其中元素递增有序。设计一个算法从A和B中的公共元素产生单链表C，要求不破坏A，B的结点
+template <typename DataType>
+DataType f_2_3_14(Node<DataType> A, Node<DataType> B)
+{
+    Node<DataType> C = new Node<DataType>;              //初始化公共链表 C
+    Node<DataType> *a = A->next, *b = B->next, *c = C;  //初始化表 A, B, C 的工作指针
+    Node<DataType> *t;                                  //初始化临时存放公共元素的指针
+    C->next = nullptr;                                  //单链表 C 置空
+    while (a != nullptr && b != nullptr)
+    {
+        if (a->data < b->data)
+            a = a->next;
+        else if (a->data >b->data)
+            b = b->next;
+        else                                            //查找到公共元素
+        {
+            t = new Node<DataType>;                     //为临时存放公共结点的指针分配存储空间
+            t->data = a->data;                          //复制公共结点数据到临时结点
+            t->next = c->next;                          //尾插法插入新结点
+            c->next = t;
+            c = c->next;                                //后移工作指针
+            a = a->next;
+            b = b->next;
+        }        
+    }
+    return C;
+}
+
+//2.3.17 (medium) 设计一个算法用于判断带头结点的循环双链表是否对称
+template <typename DataType>
+bool f_2_3_17(DNode<DataType> L)
+{
+    DNode<DataType> *p = L->next, *q = L->prior;    //两端工作指针
+    while (p != q && q->next != p)                  //指向同一结点或相邻
+    {
+        if (p->data == q->data)                     //值相同则继续比较
+        {
+            p = p->next;
+            q = q->prior;
+        }
+        else
+            return false;
+    }
+    return true;
+}
+
+//2.3.18 (medium) 有两个循环单链表，链表头指针分别为 h1 和 h2，编写一个函数将链表 h2 链接到链表 h1 之后，要求链接后的链表仍保持循环链表形式
+template <typename DataType>
+void f_2_3_18(Node<DataType> &h1, Node<DataType> &h2)
+{
+    Node<DataType> *p = h1, *q = h2;    //记录 h1 和 h2 的尾结点
+    while (p->next != h1)
+        p = p->next;
+    while (q->next != h2)
+        q = q->next;
+    p->next = h2;
+    q->next = h1;
+}
+
 int main()
 {
     int a[] = {1, 3, 5, 7, 9};
